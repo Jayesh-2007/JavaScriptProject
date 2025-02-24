d046546733b8401182fbe86f0c5b40ea003d025f
Jayesh Hadiyal <jayeshhadiyal2007@gmail.com>
2025-02-24 13:49:46 +0530

    add random choice generator function

--- RockPaperScissors/apps.js
+++ RockPaperScissors/apps.js
@@ -1,4 +1,25 @@
 let userScore = 0;
 let comptScore = 0;
 
-const choices = document.querySelector(".choices");
+const choices = document.querySelectorAll(".choice");
+
+
+const randomChoiceGen = () => {
+    const option = ["Rock","Paper","Scissors"];
+    const randomIdx = Math.floor(Math.random()*3);
+    console.log(randomIdx);
+}
+
+const playGame = (userChoice) => {
+
+}
+
+
+
+choices.forEach((choice) => {
+    console.log(choice);
+    choice.addEventListener("click",()=> {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    })
+})
