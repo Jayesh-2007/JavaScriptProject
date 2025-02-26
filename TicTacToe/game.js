ea126911eaf868ba36c9bf5e6b68c4cf2c258191
Jayesh Hadiyal <jayeshhadiyal2007@gmail.com>
2025-02-26 19:03:13 +0530

    write a logic for alternate O or X

---
+++ TicTacToe/game.js
@@ -0,0 +1,42 @@
+let boxes = document.querySelectorAll(".box"); //accesing boxes
+let resetBtn = document.querySelector(".reset"); //accesing reset button
+
+let turn0 = true;
+
+const winPatterns = [
+    [0, 1, 2], //Defining winning pattern
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click",()=> {//add eventlistner 
+        if(turn0) {
+            box.innerText="O";//if condition is true then on the box print "O"
+            turn0= false;
+        } else {
+            box.innerText="X";//if condition is false then on the box print "X"
+            turn0= true;
+        }
+        box.disabled =true;
+        checkWinner();//check winner
+    });
+});
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        if (pos1Val!=="" && pos2Val!=="" && pos3Val!=="") {
+            if (pos1Val === pos2Val && pos2Val==pos3Val) {
+                console.log("winner",pos1Val);
+            }
+        }
+    }
+}
